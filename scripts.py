9343e1ea973c07366c0d008bc3138232a9830b58
Michael-Zapivahin <zapivahin@ya.ru>
2023-04-16 09:12:15 +0300

    Dry code
    return None >> return

--- scripts.py
+++ scripts.py
@@ -1,16 +1,15 @@
 
 
 def get_subject(subject_name, year_of_study=None):
-    from django.db import models
     from datacenter.models import Subject
     try:
         subject = Subject.objects.get(title=subject_name, year_of_study=year_of_study)
     except Subject.DoesNotExist:
         print(f'Предмет {subject_name} {year_of_study} класс не найден')
-        return None
+        return
     except Subject.MultipleObjectsReturned:
         print(f'Найдено много предметов {subject_name} {year_of_study} класс')
-        return None
+        return
     return subject
 
 
@@ -32,10 +31,10 @@ def create_commendation(student_name, subject_name):
     from datacenter.models import Commendation
     from datacenter.models import Lesson
     words = ['Молодец!', 'Отлично!', 'Хорошо!', 'Гораздо лучше, чем я ожидал!', 'Ты меня приятно удивил!', 'Великолепно!', 'Прекрасно!', 'Ты меня очень обрадовал!', 'Именно этого я давно ждал от тебя!', 'Сказано здорово – просто и ясно!', 'Ты, как всегда, точен!', 'Очень хороший ответ!', 'Талантливо!', 'Ты сегодня прыгнул выше головы!', 'Я поражен!', 'Уже существенно лучше!', 'Потрясающе!', 'Замечательно!', 'Прекрасное начало!', 'Так держать!', 'Ты на верном пути!', 'Здорово!', 'Это как раз то, что нужно!', 'Я тобой горжусь!', 'С каждым разом у тебя получается всё лучше!', 'Мы с тобой не зря поработали!', 'Я вижу, как ты стараешься!', 'Ты растешь над собой!', 'Ты многое сделал, я это вижу!', 'Теперь у тебя точно все получится!']
-    class_number = 6
-    subject = get_subject(subject_name, class_number)
-    lesson = random.choice(Lesson.objects.filter(subject=get_subject(subject_name, class_number)))
-    Commendation.objects.create(text=random.choice(words), created=lesson.date, schoolkid=get_student(student_name), subject=subject, teacher=lesson.teacher)
+    student = get_student(student_name)
+    subject = get_subject(subject_name, student.year_of_study)
+    lesson = random.choice(Lesson.objects.filter(subject=subject))
+    Commendation.objects.create(text=random.choice(words), created=lesson.date, schoolkid=student, subject=subject, teacher=lesson.teacher)
 
 
 def update_mark(student_name, subject_name):
@@ -45,9 +44,6 @@ def update_mark(student_name, subject_name):
     Mark.objects.filter(schoolkid=student, subject=subject, points__in=[2, 3]).update(points=5)
 
 
-
 def delete_chastisements(student_name):
     from datacenter.models import Chastisement
     Chastisement.objects.filter(schoolkid=get_student(student_name)).delete()
-
-
